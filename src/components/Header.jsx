6fa65d7b01abd5062e89dbe39f85f41530e6160d
DhoriRohit1 <rohitdhori3@gmail.com>
2024-10-04 19:46:38 -0700

    Initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className="bg-info text-light p-3 pl-5">
+            <h1 style={{marginLeft:"160px"}}>Add Recipe</h1>
+        </header>
+    );
+};
+
+export default Header;
